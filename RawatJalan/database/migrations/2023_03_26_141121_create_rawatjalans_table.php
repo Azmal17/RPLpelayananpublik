88c3dd1e3d09517debd31f152e974b1cb7dae324
Dafa15 <dafaaldian25@gmail.com>
2023-03-27 22:05:56 +0700

    Rawat Jalan (M. Dafa Aldian)

---
+++ RawatJalan/database/migrations/2023_03_26_141121_create_rawatjalans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rawatjalans', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('norm');
+            $table->bigInteger('noregistrasi');
+            $table->bigInteger('norujukan');
+            $table->string('namapasien');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rawatjalans');
+    }
+};
