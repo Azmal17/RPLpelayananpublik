bece36a7bc4336cde4288278a27ffef669e1506c
malindard <malindard@students.unnes.ac.id>
2023-04-10 20:41:54 +0700

    add navbar di rawat inap

--- resources/views/inaps/layout.blade.php
+++ resources/views/inaps/layout.blade.php
@@ -1,14 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Pendaftaran Rawat Inap</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
+
+@extends('layout.admin')
+
+@section('content')
 <body>
+<br>
+<br>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     
 <div class="container">
     @yield('content')
 </div>
     
 </body>
-</html>
+
+@endsection
