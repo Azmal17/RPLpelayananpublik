eb8516d4635ea7baf7b77f1eddffdd5b8e140105
Dafa15 <dafaaldian25@gmail.com>
2023-04-10 20:15:54 +0700

    fix loadscreen datarawatjalan

--- resources/views/rawatjalan.blade.php
+++ resources/views/rawatjalan.blade.php
@@ -29,44 +29,44 @@
     </div><!-- /.container-fluid -->
   </div>
   <div class="container">
-      <div class="row">
-          <a href="/tambahdatarawatjalan" class="btn btn-primary" >Tambah</a>
-          @if ($message = Session::get('success'))
-              <div class="alert alert-danger" role="alert">
-                  {{$message}}
-              </div> 
-          @endif
-          <table class="table">
-              <thead>
-                <tr>
-                  <th scope="col">No</th>
-                  <th scope="col">No RM</th>
-                  <th scope="col">No Registrasi</th>
-                  <th scope="col">No Rujukan</th>
-                  <th scope="col">Nama Pasien</th>
-                  <th scope="col">Aksi</th>
-                </tr>
-              </thead>
-              <tbody>
-                @php
-                  $no = 1;
-                @endphp 
-                @foreach ($data as $index => $row)
-                  <tr>
-                    <th scope="row">{{$row->id}}</th>
-                    <td>{{$row->norm}}</td>
-                    <td>{{$row->noregistrasi}}</td>
-                    <td>{{$row->norujukan}}</td>
-                    <td>{{$row->namapasien}}</td>
-                    <td>
-                        <a href="/tampilkandatarawatjalan/{{$row->id}}" class="btn btn-secondary">Edit</a>
-                        <a href="#" class="btn btn-danger delete" data-id="{{$row->id}}" data-norm="{{$row->norm}}">Delete</a>
-                    </td>
-                  </tr>
-                @endforeach
-              </tbody>
-            </table>
-      </div>
+    <a href="/tambahdatarawatjalan" class="btn btn-primary" >Tambah</a>
+    <div class="row">
+      @if ($message = Session::get('success'))
+          <div class="alert alert-danger" role="alert">
+              {{$message}}
+          </div> 
+      @endif
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">No</th>
+            <th scope="col">No RM</th>
+            <th scope="col">No Registrasi</th>
+            <th scope="col">No Rujukan</th>
+            <th scope="col">Nama Pasien</th>
+            <th scope="col">Aksi</th>
+          </tr>
+        </thead>
+        <tbody>
+          @php
+            $no = 1;
+          @endphp 
+          @foreach ($data as $index => $row)
+            <tr>
+              <th scope="row">{{$row->id}}</th>
+              <td>{{$row->norm}}</td>
+              <td>{{$row->noregistrasi}}</td>
+              <td>{{$row->norujukan}}</td>
+              <td>{{$row->namapasien}}</td>
+              <td>
+                  <a href="/tampilkandatarawatjalan/{{$row->id}}" class="btn btn-secondary">Edit</a>
+                  <a href="#" class="btn btn-danger delete" data-id="{{$row->id}}" data-norm="{{$row->norm}}">Delete</a>
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
   </div>
 </div>
 
