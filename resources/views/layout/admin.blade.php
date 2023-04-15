0cca643b8330da2d9dac607eabe7263e1a0d469d
Dafa15 <dafaaldian25@gmail.com>
2023-04-15 17:36:36 +0700

    Add link cssfont

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -6,7 +6,7 @@
   <title>Pelayanan Publik ROSITA</title>
 
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="<script src="https://kit.fontawesome.com/2d26295c33.js" crossorigin="anonymous"></script>">
+  <link rel="stylesheet" href="{{asset('fontawesome/all.css')}}">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="{{ asset('template/plugins/fontawesome-free/css/all.min.css') }}">
