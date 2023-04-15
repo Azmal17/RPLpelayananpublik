8b8f34ce93f607ff94af09fddd7287f6fa7f6bc0
Dafa15 <dafaaldian25@gmail.com>
2023-04-15 17:40:05 +0700

    addfont

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -6,7 +6,7 @@
   <title>Pelayanan Publik ROSITA</title>
 
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="{{asset('fontawesome/all.css')}}">
+  <link rel="stylesheet" href="{{asset('fontawesome/css/all.css')}}">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="{{ asset('template/plugins/fontawesome-free/css/all.min.css') }}">
