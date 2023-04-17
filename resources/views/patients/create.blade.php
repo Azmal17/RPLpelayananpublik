4c9d1c3bee7b5b3172bbc53514fa381d3ac26a94
HanaDewi <hanadewi@students.unnes.ac.id>
2023-04-17 23:10:10 +0700

    add <form

--- resources/views/patients/create.blade.php
+++ resources/views/patients/create.blade.php
@@ -12,6 +12,21 @@
             </div>
         </div>
 
+        <form action="{{ route('patients.store') }}" method="POST">
+            @csrf
+        <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>No RM:</strong>
+                    <input type="text" name="no_rm" class="form-control" placeholder="No RM">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Nomor Asuransi:</strong>
+                    <input type="text" name="no_asuransi" class="form-control" placeholder="Nomor Asuransi">
+                </div>
+            </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Nama Pasien:</strong>
@@ -34,8 +49,8 @@
             <div class="form-group">
                 <strong>Jenis Kelamin:</strong>
                 <select name="jenis_kelamin" class="form-control">
-                    <option value="Laki-laki">Laki-laki</option>
-                    <option value="Perempuan">Perempuan</option>
+                    <option value="Pria">Pria</option>
+                    <option value="Wanita">Wanita</option>
                 </select>
             </div>
         </div>
@@ -101,100 +116,7 @@
                 <button type="submit" class="btn btn-primary">Submit</button>
         </div>
     </div>
-    
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    
-    <form action="{{ route('patients.store') }}" method="POST">
-        @csrf
-    
-        <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>No RM:</strong>
-                    <input type="text" name="no_rm" class="form-control" placeholder="No RM">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Nomor Asuransi:</strong>
-                    <input type="text" name="no_asuransi" class="form-control" placeholder="Nomor Asuransi">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Nama Pasien:</strong>
-                    <input type="text" name="nama_pasien" class="form-control" placeholder="Nama Pasien">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Umur:</strong>
-                    <input type="text" name="umur" class="form-control" placeholder="Umur">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Tanggal Lahir:</strong>
-                    <input type="text" name="tanggal_lahir" class="form-control" placeholder="Tanggal Lahir">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Jenis Kelamin:</strong>
-                    <input type="text" name="jenis_kelamin" class="form-control" placeholder="Jenis Kelamin">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Alamat Pasien:</strong>
-                    <input type="text" name="alamat_pasien" class="form-control" placeholder="Alamat Pasien">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Golongan Darah:</strong>
-                    <input type="text" name="gol_darah" class="form-control" placeholder="Golongan Darah">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Nomor Telepon:</strong>
-                    <input type="text" name="no_telp" class="form-control" placeholder="Nomor Telepon">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Email</strong>
-                    <input type="text" name="email" class="form-control" placeholder="Email">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Agama:</strong>
-                    <input type="text" name="agama" class="form-control" placeholder="Agama">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Status Kawin:</strong>
-                    <input type="text" name="status_kawin" class="form-control" placeholder="Status Kawin">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </div>
-        </div>
-    
+
     </form>
 </div>
 @endsection
