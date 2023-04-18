56426864295ac06a1c37be514b529619c586e7fd
Dafa15 <dafaaldian25@gmail.com>
2023-04-18 07:02:33 +0700

    rollback

--- resources/views/tambahdata.blade.php
+++ resources/views/tambahdata.blade.php
@@ -31,6 +31,7 @@
             <div class="mb-3">
               <label for="exampleInputEmail1" class="form-label">Jenis Kelamin</label>
               <select class="form-select" name="jeniskelamin" aria-label=".form-select-sm example">
+                <option selected>Pilih Jenis Kelamin</option>
                 <option value="Pria">Pria</option>
                 <option value="Wanita">Wanita</option>
               </select>
