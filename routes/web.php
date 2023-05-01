ffdf92275a314fc025b3cdc27209b63bf99ce6a2
Dafa15 <dafaaldian25@gmail.com>
2023-05-02 03:26:41 +0700

    add pendaftaranuser2

--- routes/web.php
+++ routes/web.php
@@ -89,6 +89,7 @@ Route::get('/pengaduanuser',[LoginUserController::class, 'pengaduan']);
 Route::get('/faq',[LoginUserController::class, 'faq']);
 Route::get('/pri',[LoginUserController::class, 'pri']);
 Route::get('/prj',[LoginUserController::class, 'prj']);
+Route::get('/pendaftaranrawatjalan',[LoginUserController::class, 'pendaftaranrawatjalan'])->name ('pendaftaranrawatjalan');
 
 //Google Login
 Route::get('/auth/google', [App\Http\Controllers\GoogleController::class, 'redirectToGoogle'])->name('google.login');
