27e3499166b7734e953d950a546372a04eb82ea0
malindard <malindard@students.unnes.ac.id>
2023-05-09 01:04:21 +0700

    fix antrian

--- resources/views/loginuser/antrian.blade.php
+++ resources/views/loginuser/antrian.blade.php
@@ -8,11 +8,8 @@
   <div class="card-body">
     <h5 class="card-title">Nomor Antrian</h5>
     <h3>Nomor Antrian</h3>
-    <h2>{{$no_antrian}}</h2>
     <h3>Tanggal Kunjungan</h3>
-    <h2>{{$tanggal_kunjungan}}</h2>
     <h3>Klinik</h3>
-    <h2>{{$klinik}}</h2>
     <a href="#" class="btn btn-primary">Download</a>
   </div>
 </div>
