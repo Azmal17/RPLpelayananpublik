b18d74c396e6b92c75a6b83e1ccc44f5876655f0
Dafa15 <dafaaldian25@gmail.com>
2023-05-08 20:25:36 +0700

    fix delete button rawatjalan

--- resources/views/rawatjalan.blade.php
+++ resources/views/rawatjalan.blade.php
@@ -69,9 +69,6 @@
     </div>
   </div>
 </div>
-
-@endsection
-
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.6.4.slim.js" integrity="sha256-dWvV84T6BhzO4vG6gWhsWVKVoa4lVmLnpBOZh/CAHU4=" crossorigin="anonymous"></script>
@@ -107,3 +104,5 @@
         });
     });
   </script>
+@endsection
+
