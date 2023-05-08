3b226e7b76497a9126b842e5509c69090b6c7d9d
inzhani97 <suginiinjani@students.unnes.ac.id>
2023-05-08 22:39:41 +0700

    tambah rut klinik

--- resources/views/patients/create.blade.php
+++ resources/views/patients/create.blade.php
@@ -113,7 +113,7 @@
         </div>
 
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" href="/pilihklinik" class="btn btn-primary">Submit</button>
         </div>
     </div>
 
