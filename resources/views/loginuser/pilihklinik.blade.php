537dfc42020dc8340cdec459bd45b31deac10afb
malindard <malindard@students.unnes.ac.id>
2023-05-09 01:55:27 +0700

    fix pengaduan user

--- resources/views/loginuser/pilihklinik.blade.php
+++ resources/views/loginuser/pilihklinik.blade.php
@@ -7,15 +7,9 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
   integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-      <div class="col-lg-12 margin-tb">
-      <div>
-          <h2 class= "text-center">Antrian</h2>
-      </div>
-      <div class="pull-right">
-          <a class="btn btn-primary" href="/pendaftaranuser.create"> Back</a>
-      </div>
-      </div>
-
+    <h1 class="text-center">Pilih Klinik</h1>
+        <div class="container">
+            <button type="button" class="btn btn-primary">Back</button>
             <div class="row justify-content-center">
               <div class="col-8">
                 <div class="card">
