4bf4f37ed0b8186aee698b25c1c66b6b8451250a
inzhani97 <suginiinjani@students.unnes.ac.id>
2023-05-09 00:50:47 +0700

    ubah antrian

--- resources/views/loginuser/antrian.blade.php
+++ resources/views/loginuser/antrian.blade.php
@@ -7,10 +7,12 @@
   </div>
   <div class="card-body">
     <h5 class="card-title">Nomor Antrian</h5>
-    <p class="card-text">No:<br/>
-    Tanggal Kunjungan: <br/>
-    Klinik: <br/>
-    </p>
+    <h3>Nomor Antrian</h3>
+    <h2>{{$no_antrian}}</h2>
+    <h3>Tanggal Kunjungan</h3>
+    <h2>{{$tanggal_kunjungan}}</h2>
+    <h3>Klinik</h3>
+    <h2>{{$klinik}}</h2>
     <a href="#" class="btn btn-primary">Download</a>
   </div>
 </div>
