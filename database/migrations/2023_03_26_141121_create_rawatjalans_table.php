606d19682f19c771ee4cd1c4bb2d6adc3849ac1d
Dafa15 <dafaaldian25@gmail.com>
2023-05-15 21:55:03 +0700

    fix migration

--- database/migrations/2023_03_26_141121_create_rawatjalans_table.php
+++ database/migrations/2023_03_26_141121_create_rawatjalans_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('rawatjalans', function (Blueprint $table) {
-            $table->id('');
+            $table->id();
             $table->bigInteger('noregistrasi');
             $table->bigInteger('norm');
             $table->bigInteger('norujukan');
