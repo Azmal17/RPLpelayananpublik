fd173d08a3f30cabbf6f2ecc9f12aaf0a486b979
inzhani97 <suginiinjani@students.unnes.ac.id>
2023-05-23 09:05:30 +0700

    ubah

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -39,7 +39,7 @@
 
         <div class="d-flex align-items-center justify-content-between">
             <a href="index.html" class="logo d-flex align-items-center">
-                <img src="{{ asset('assets') }}/img/p.png" alt="">
+                <img src="{{ asset('assets') }}/img/p.png" alt="p">
                 <span class="d-none d-lg-block">Rumah Sakit</span>
             </a>
             <i class="bi bi-list toggle-sidebar-btn"></i>
