10c8d743087373fa5f91b2e2a186fdfa2cceb83f
Dafa15 <dafaaldian25@gmail.com>
2023-06-08 17:59:53 +0700

    add link antrian ti layout

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -233,7 +233,7 @@
           </li> --}}
 
           <li class="nav-item">
-            <a href="/antrian" class="nav-link">
+            <a href="/" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
